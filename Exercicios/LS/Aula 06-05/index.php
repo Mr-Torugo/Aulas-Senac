1f1e0f9a25d42b8912447f20852506d86184c10d
vitor hugo <vhugo4040>
2026-05-13 20:50:13 -0300

    aula 13/05

--- Exercicios/LS/Aula 06-05/index.php
+++ Exercicios/LS/Aula 06-05/index.php
@@ -8,12 +8,31 @@
    //O resultado (cursors da consulta) fica guardado em $stmt(statement)
    $stmt = $pdo -> query ("SELECT * FROM contatos ORDER BY id ASC");
    
-
+ 
    //Pega todas as linhas retornadas pela consulta e transforma em um array de registros (cada posição no array é um contato)
    //No final, $contatos vira um array de contatos
    $contatos = $stmt -> fetchAll();
+ 
+   //exclusao via post
+   //filtrando campo delete id para validar se o tipo dele é inteir
+ 
+   if($_SERVER['REQUEST_METHOD']==="POST" && isset($_POST["delete_id"])){
+    $id=filter_input(INPUT_POST,'delete_id',FILTER_VALIDATE_INT);
+    //echo $id;
+    //die();
+ 
+    if($id){
+        // prepara o SQL para apagar  o registro
+        $stmt= $pdo -> prepare("DELETE FROM contatos WHERE id = : id");
+        //excutar o SQL
+        stmt -> execute([':id'=> $id]);
+ 
+    }
+   }
+   header("Location: /");
+   exit
 ?>
-
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,7 +43,7 @@
 <body>
     <?php
     if(empty($contatos)):?>
-    <p>Nenhum registor encontrado</p>
+    <p>Nenhum registro encontrado</p>
     <?php else: ?>
     <table border="1" cellpadding="1" cellspacing="0">
     <thead >
@@ -35,17 +54,25 @@
         </tr>
     </thead>
     <tbody>
-        <?php
-        foreach($contatos as $contato)
-        ?>
+        <?php foreach($contatos as $contato) : ?>
         <tr>
-            <td><?=htmlespecialchars($contato['id'])?></td>
-            <td><?=htmlespecialchars($contato['nome'])?></td>
-            <td><?=htmlespecialchars($contato['email'])?></td>
-            <td>Atualizar</td>
-            <td>Excluir</td>
+            <td><?=htmlspecialchars($contato['id'])?></td>
+            <td><?=htmlspecialchars($contato['nome'])?></td>
+            <td><?=htmlspecialchars($contato['email'])?></td>
+            <td>
+                <form method="post" action="/form_contato.php">
+                    <input type="hidden" name="atualiza_id" value="<?=$contato["id"];?>">
+                <button type="submit">Atualizar</button>
+                </form>
+            </td>
+            <td>
+                <form method="post" action="">
+                    <input type="hidden" name="delete_id" value="<?=$contato["id"];?>">
+                <button type="submit">Excluir</button>
+                </form>
+            </td>
         </tr>
-        <?php endforeach?>
+        <?php endforeach ?>
     </tbody>
     </table>
     <?php endif ?>
